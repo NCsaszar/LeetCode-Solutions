aa325a14301117c0d3dc3aebd9fde4b3bde3c8ba
Nicholas Csaszar <113650119+NCsaszar@users.noreply.github.com>
2023-05-27 16:20:24 -0400

    Time: 72 ms (31.30%), Space: 44.7 MB (25.52%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+function canPlaceFlowers(flowerbed, n) {
+  let count = 0;
+  let i = 0;
+
+  while (i < flowerbed.length) {
+    if (
+      flowerbed[i] === 0 &&
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      flowerbed[i] = 1; // Plant a flower
+      count++;
+
+      if (count >= n) {
+        return true; // Already planted enough flowers
+      }
+    }
+
+    i++;
+  }
+
+  return count >= n;
+}
