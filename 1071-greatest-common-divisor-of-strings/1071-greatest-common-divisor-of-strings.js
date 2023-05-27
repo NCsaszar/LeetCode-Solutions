85b6ae53705cbaa6e783b76ce00ccee44fee2933
Nicholas Csaszar <113650119+NCsaszar@users.noreply.github.com>
2023-05-27 15:30:10 -0400

    Time: 66 ms (42.58%), Space: 42.3 MB (72.15%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) return '';
+
+  let gcdLen = gcd(str1.length, str2.length);
+
+  return str1.substring(0, gcdLen);
+};
+
+let gcd = function (a, b) {
+  while (b != 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
