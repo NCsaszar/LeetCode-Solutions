4f94de99b9fb01a71225312609adf4c367fc8727
Nicholas Csaszar <113650119+NCsaszar@users.noreply.github.com>
2023-11-01 12:07:26 -0400

    Time: 58 ms (69.07%), Space: 44.2 MB (77.27%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -2,33 +2,28 @@
  * @param {character[]} chars
  * @return {number}
  */
-var compress = function (chars) {
-  let write = 0;
+var compress = function(chars) {
+    let write = 0, read = 0; // initialize pointers
 
-  for (let read = 0; read < chars.length; ) {
-    let count = 0;
-    let ch = chars[read];
+    while (read < chars.length) {
+        let ch = chars[read];
+        let count = 0;
 
-    //count repeating chars
-    let j = read;
-    while (j < chars.length && chars[j] === ch) {
-      count++;
-      j++;
+        // Count consecutive repeating characters
+        while (read < chars.length && chars[read] === ch) {
+            read++;
+            count++;
+        }
+
+        // Write character and its count if > 1
+        chars[write++] = ch;
+        if (count > 1) {
+            for (let c of String(count)) {
+                chars[write++] = c;
+            }
+        }
     }
-
-    //read will now start where last consecutive character ended
-    read = j;
-
-    //write the character and its count if > 1
-    chars[write] = ch;
-    write++;
-    if (count > 1) {
-      let countStr = String(count);
-      for (let k = 0; k < countStr.length; k++) {
-        chars[write] = countStr[k];
-        write++;
-      }
-    }
-  }
-  return write;
+    
+    return write;
 };
+
