948e2f060960ac34e38f524127c75b183a201fc6
Nicholas Csaszar <113650119+NCsaszar@users.noreply.github.com>
2023-11-02 12:53:01 -0400

    Time: 116 ms (99.39%), Space: 51.3 MB (81.74%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.js
+++ 0816-design-hashset/0816-design-hashset.js
@@ -1,35 +1,65 @@
-var MyHashSet = function () {
-  this.hash = new Array(10 ** 6 + 1);
-};
+//node definition
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class MyHashSet {
+  constructor() {
+    this.size = 1000;
+    this.set = new Array(this.size).fill(null);
+  }
 
-/**
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.add = function (key) {
-  this.hash[key] = 1;
-};
+  hash(key) {
+    return key % this.size;
+  }
 
-/**
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.remove = function (key) {
-  this.hash[key] = 0;
-};
+  add(key) {
+    let idx = this.hash(key);
 
-/**
- * @param {number} key
- * @return {boolean}
- */
-MyHashSet.prototype.contains = function (key) {
-  return this.hash[key] === 1;
-};
+    if (this.set[idx] === null) {
+      this.set[idx] = new Node(key);
+      return;
+    }
+    let currentNode = this.set[idx];
+    while (currentNode) {
+      if (currentNode.value === key) return;
+      if (currentNode.next === null) break;
+      currentNode = currentNode.next;
+    }
+    currentNode.next = new Node(key);
+  }
 
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * var obj = new MyHashSet()
- * obj.add(key)
- * obj.remove(key)
- * var param_3 = obj.contains(key)
- */
+  remove(key) {
+    let idx = this.hash(key);
+    let currentNode = this.set[idx];
+
+    if (currentNode === null) return;
+    if (currentNode.value === key) {
+      this.set[idx] = currentNode.next;
+      return;
+    }
+
+    while (currentNode.next) {
+      if (currentNode.next.value === key) {
+        currentNode.next = currentNode.next.next;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  contains(key) {
+    let idx = this.hash(key);
+    let currentNode = this.set[idx];
+
+    if (!currentNode) return false;
+
+    while (currentNode) {
+      if (currentNode.value === key) return true;
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+}
