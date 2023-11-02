5db3b88de0cdb4af9af4ec932f3106db7bd98938
Nicholas Csaszar <113650119+NCsaszar@users.noreply.github.com>
2023-11-02 14:35:33 -0400

    Time: 63 ms (34.94%), Space: 44.3 MB (66.63%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+  let output = [];
+  for (let i = 0; i < nums.length / 2; i++) {
+    output.push(nums[i]);
+    output.push(nums[i + n]);
+  }
+  return output;
+};
